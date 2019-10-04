a615b9e06072e87a277f4319b9c55b39fd18fb81
Piotr <decoder@live.de>
2019-10-04 14:10:22 +0200

    More config

--- google/main.tf
+++ google/main.tf
@@ -3,3 +3,25 @@ provider "google" {
   region  = "europe-west3"
   zone    = "europe-west3-a"
 }
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "${google_compute_network.vpc_network.self_link}"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
