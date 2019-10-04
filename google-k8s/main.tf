28486f3a6efb612b58284836003d1ae9410f0995
Piotr <decoder@live.de>
2019-10-04 15:07:19 +0200

    initial k8s cluster setup

---
+++ google-k8s/main.tf
@@ -0,0 +1,42 @@
+provider "google" {
+  credentials = "${file("..\\TerraformKey.json")}"
+  project = "atos-dev-254809"
+  region  = "europe-west3"
+  zone    = "europe-west3-a"
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "atos-dev"
+  location           = "europe-west3-a"
+  initial_node_count = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "atos-node-pool"
+  location   = "europe-west3-a"
+  cluster    = "${google_container_cluster.primary.name}"
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
