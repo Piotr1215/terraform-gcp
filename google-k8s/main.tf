30fcd19975059f1852f831849b72b810413140c6
Piotr <decoder@live.de>
2019-11-02 13:34:27 +0100

    Introduced variables in terraform setup

--- google-k8s/main.tf
+++ google-k8s/main.tf
@@ -1,46 +1,81 @@
 provider "google" {
   credentials = "${file("..\\TerraformKey.json")}"
-  project     = "atos-dev-254809"
-  region      = "europe-west3"
-  zone        = "europe-west3-a"
+  project     = "${var.project}"
+  region      = "${var.region}"
+  zone        = "${var.cluster_zone}"
 }
 
-resource "google_container_cluster" "primary" {
-  name               = "atos-dev"
-  location           = "europe-west3-a"
-  initial_node_count = 1
-
+resource "google_container_cluster" "cluster" {
+  name               = "${var.cluster_name}"
+  location           = "${var.cluster_zone}"
+  min_master_version = "${var.cluster_k8s_version}"
+  initial_node_count = "${var.initial_node_count}"
+  
   master_auth {
     username = ""
     password = ""
 
     client_certificate_config {
-      issue_client_certificate = false
+      issue_client_certificate = true
     }
   }
 }
 
-resource "google_container_node_pool" "primary_preemptible_nodes" {
+resource "google_container_node_pool" "primary_regular_nodes" {
   name       = "atos-node-pool"
-  location   = "europe-west3-a"
-  cluster    = "${google_container_cluster.primary.name}"
-  node_count = 3
+  location   = "${var.cluster_zone}"
+  cluster    = "${google_container_cluster.cluster.name}"
+  initial_node_count = "${var.initial_node_count}"
+
+  management {
+      auto_repair = true
+    }
+
+  autoscaling {
+    min_node_count = "${var.autoscaling_min_node_count}"
+    max_node_count = "${var.autoscaling_max_node_count}"
+  }
 
   node_config {
-    preemptible  = true
-    machine_type = "n1-standard-1"
+    preemptible  = false
+    disk_size_gb = "${var.disk_size_gb}"
+    disk_type    = "${var.disk_type}"
+    machine_type = "${var.machine_type}"
 
     metadata = {
       disable-legacy-endpoints = "true"
     }
 
     oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+        "https://www.googleapis.com/auth/service.management.readonly",
+        "https://www.googleapis.com/auth/servicecontrol",
+        "https://www.googleapis.com/auth/trace.append",
+        "https://www.googleapis.com/auth/compute",
     ]
   }
-
 }
-  output "name" {
-    value = "${google_container_cluster.primary.name}"
-  }
+ #
+# Output for K8S
+#
+output "client_certificate" {
+  value = "${google_container_cluster.cluster.master_auth.0.client_certificate}"
+  sensitive = true
+}
+
+output "client_key" {
+  value = "${google_container_cluster.cluster.master_auth.0.client_key}"
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value = "${google_container_cluster.cluster.master_auth.0.cluster_ca_certificate}"
+  sensitive = true
+}
+
+output "host" {
+  value = "${google_container_cluster.cluster.endpoint}"
+  sensitive = true
+}
---
+++ google-k8s/variables.tf
@@ -0,0 +1,45 @@
+#
+# Variables
+#
+variable "project" {
+  default = "atos-dev-254809"
+}
+variable "region" {
+  default = "europe-west3"
+}
+
+variable "cluster_name" {
+  default = "atos-dev-k8s"
+}
+
+variable "cluster_zone" {
+  default = "europe-west3-a"
+}
+
+variable "cluster_k8s_version" {
+  default = "1.14.7-gke.14"
+}
+
+variable "initial_node_count" {
+  default = 1
+}
+
+variable "autoscaling_min_node_count" {
+  default = 1
+}
+
+variable "autoscaling_max_node_count" {
+  default = 3
+}
+
+variable "disk_size_gb" {
+  default = 10
+}
+
+variable "disk_type" {
+  default = "pd-standard"
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
