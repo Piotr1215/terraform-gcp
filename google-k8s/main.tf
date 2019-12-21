c09dd76d11cac0b6c364f36e68270fcda8e2b534
decoder <piotrzan@gmail.com>
2019-12-22 00:22:17 +0100

    fixed interpolation

--- google-k8s/main.tf
+++ google-k8s/main.tf
@@ -1,16 +1,16 @@
 provider "google" {
-  credentials = "${file("..\\TerraformKey.json")}"
-  project     = "${var.project}"
-  region      = "${var.region}"
-  zone        = "${var.cluster_zone}"
+  credentials = file("../TerraformKey.json")
+  project     = var.project
+  region      = var.region
+  zone        = var.cluster_zone
 }
 
 resource "google_container_cluster" "cluster" {
-  name               = "${var.cluster_name}"
-  location           = "${var.cluster_zone}"
-  min_master_version = "${var.cluster_k8s_version}"
-  initial_node_count = "${var.initial_node_count}"
-  
+  name               = var.cluster_name
+  location           = var.cluster_zone
+  min_master_version = var.cluster_k8s_version
+  initial_node_count = var.initial_node_count
+
   master_auth {
     username = ""
     password = ""
@@ -23,24 +23,24 @@ resource "google_container_cluster" "cluster" {
 
 resource "google_container_node_pool" "primary_regular_nodes" {
   name       = "atos-node-pool"
-  location   = "${var.cluster_zone}"
-  cluster    = "${google_container_cluster.cluster.name}"
-  initial_node_count = "${var.initial_node_count}"
+  location   = var.cluster_zone
+  cluster    = google_container_cluster.cluster.name
+  initial_node_count = var.initial_node_count
 
   management {
       auto_repair = true
     }
 
   autoscaling {
-    min_node_count = "${var.autoscaling_min_node_count}"
-    max_node_count = "${var.autoscaling_max_node_count}"
+    min_node_count = var.autoscaling_min_node_count
+    max_node_count = var.autoscaling_max_node_count
   }
 
   node_config {
     preemptible  = false
-    disk_size_gb = "${var.disk_size_gb}"
-    disk_type    = "${var.disk_type}"
-    machine_type = "${var.machine_type}"
+    disk_size_gb = var.disk_size_gb
+    disk_type    = var.disk_type
+    machine_type = var.machine_type
 
     metadata = {
       disable-legacy-endpoints = "true"
@@ -61,21 +61,21 @@ resource "google_container_node_pool" "primary_regular_nodes" {
 # Output for K8S
 #
 output "client_certificate" {
-  value = "${google_container_cluster.cluster.master_auth.0.client_certificate}"
+  value = google_container_cluster.cluster.master_auth.0.client_certificate
   sensitive = true
 }
 
 output "client_key" {
-  value = "${google_container_cluster.cluster.master_auth.0.client_key}"
+  value = google_container_cluster.cluster.master_auth.0.client_key
   sensitive = true
 }
 
 output "cluster_ca_certificate" {
-  value = "${google_container_cluster.cluster.master_auth.0.cluster_ca_certificate}"
+  value = google_container_cluster.cluster.master_auth.0.cluster_ca_certificate
   sensitive = true
 }
 
 output "host" {
-  value = "${google_container_cluster.cluster.endpoint}"
+  value = google_container_cluster.cluster.endpoint
   sensitive = true
 }
